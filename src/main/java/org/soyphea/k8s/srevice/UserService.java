a2897ee6a4c313aa02a8a20f48d114a6cc5b48fc
dvilchesbcn <davisvil@gmail.com>
2022-10-25 14:54:01 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -6,11 +6,25 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.crypto.Cipher;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
+
 @Service
 public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-
+    
+    public static void main(String[] args) {
+        try
+        {
+            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+        }
+        catch(NoSuchAlgorithmException|NoSuchPaddingException e)
+        {
+        }
+    }
+    
     public List<User> getUser(String containName) {
         String inputPasword = "en duro!!!! nooo!!";
 
