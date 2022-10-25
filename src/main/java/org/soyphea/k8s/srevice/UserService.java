bda70c405c2b84f122b3f95f7558d0b926fd50ac
dvilchesbcn <davisvil@gmail.com>
2022-10-25 15:39:24 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -6,30 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.crypto.Cipher;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.NoSuchPaddingException;
-
 @Service
 public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
     
-    public static void main(String[] args) {
-        try
-        {
-            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
-        }
-        catch(NoSuchAlgorithmException|NoSuchPaddingException e)
-        {
-        }
-    }
-    
     public List<User> getUser(String containName) {
-        String inputPasword = "en duro!!!! nooo!!";
-
-        String query = "SELECT * FROM users WHERE user = '" + containName + "' AND pass = '" + inputPasword + "'"; // Unsafe
-
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
 
