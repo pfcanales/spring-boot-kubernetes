325383cfde1735ca6a05055b4525270a0bad4564
pfcanales <pfcanales@gmail.com>
2022-10-26 14:48:57 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -13,18 +13,11 @@ public class UserService {
        
     public List<User> getUser(String containName) {
 
+        KeyPairGenerator keyPairGen1 = KeyPairGenerator.getInstance("RSA");
+        keyPairGen1.initialize(1024); // Noncompliant
+        
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
     
-    // Set up the environment for creating the initial context
-    Hashtable<String, Object> env = new Hashtable<String, Object>();
-    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-    env.put(Context.PROVIDER_URL, "ldap://localhost:389/o=JNDITutorial");
-
-    // Use anonymous authentication
-    env.put(Context.SECURITY_AUTHENTICATION, "none"); // Noncompliant
-
-    // Create the initial context
-    DirContext ctx = new InitialDirContext(env);
 
 }
