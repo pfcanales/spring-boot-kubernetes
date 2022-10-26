fa1b5513c0eabb93eb1e4393baf1b2dbf1d57abb
pfcanales <pfcanales@gmail.com>
2022-10-26 11:35:47 -0300

    Update ControllerTest.java

--- src/test/java/org/soyphea/k8s/ControllerTest.java
+++ src/test/java/org/soyphea/k8s/ControllerTest.java
@@ -43,3 +43,11 @@ public class ControllerTest {
     }
 
 }
+protected void finalize() {   // Noncompliant; no call to super.finalize();
+  releaseSomeResources();
+}
+
+protected void finalize() {
+  super.finalize();  // Noncompliant; this call should come last
+  releaseSomeResources();
+}
