5bea01747518d57480e3574f075f29a6f0c0940a
pfcanales <pfcanales@gmail.com>
2022-10-26 13:23:41 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -15,17 +15,4 @@ public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
        
-    public List<User> getUser(String containName) {
-
-        return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
-    }
-    
-    public static void main(String[] args) {
-      try
-      {
-        Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
-        Cipher c7 = Cipher.getInstance("DESede"); // Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
-      }
-    }
-
 }
