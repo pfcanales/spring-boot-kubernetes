5813c9b86f2d71ca544b09a3f7425db590853558
pfcanales <pfcanales@gmail.com>
2022-10-26 14:41:19 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -16,9 +16,14 @@ public class UserService {
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
     
-    File tempDir;
-    tempDir = File.createTempFile("", ".");
-    tempDir.delete();
-    tempDir.mkdir();  // Noncompliant
+    protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+        .antMatchers("/resources/**", "/signup", "/about").permitAll() // Compliant
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/admin/login").permitAll() // Noncompliant; the pattern "/admin/login" should appear before "/admin/**"
+        .antMatchers("/**", "/home").permitAll()
+        .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") // Noncompliant; the pattern "/db/**" should occurs before "/**"
+        .and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+    }
 
 }
