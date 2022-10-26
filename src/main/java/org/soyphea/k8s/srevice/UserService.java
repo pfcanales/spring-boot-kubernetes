4d5eac0598a9de0ba7607b5b985e998fa145f634
pfcanales <pfcanales@gmail.com>
2022-10-26 12:09:40 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -11,6 +11,8 @@ public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
 
+    String password='12345'
+        
     public List<User> getUser(String containName) {
 
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
