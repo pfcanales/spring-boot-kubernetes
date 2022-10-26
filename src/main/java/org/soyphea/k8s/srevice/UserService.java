186ef4e5e44643e673fcd01dcd56081625bd0de6
pfcanales <pfcanales@gmail.com>
2022-10-26 13:24:37 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -14,5 +14,12 @@ import javax.crypto.NoSuchPaddingException;
 public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-       
+
+    String password='12345'
+        
+    public List<User> getUser(String containName) {
+
+        return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
+    }
+
 }
