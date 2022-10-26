25c1efda67c17f1e6d34eadd762e83b5a1119884
pfcanales <pfcanales@gmail.com>
2022-10-26 14:14:27 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -10,12 +10,14 @@ import java.util.stream.Collectors;
 public class UserService {  
     
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-       
+    
+    String key = "gb09ym9ydoolp3w886d0tciczj6ve9kszqd65u7d126040gwy86xqimjpuuc788g";
+    SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("test.db", key, null); // Noncompliant
+    
     public List<User> getUser(String containName) {
 
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
-
     
 
 }
