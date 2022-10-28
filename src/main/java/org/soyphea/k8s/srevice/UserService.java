3e4c7d9bf1078543c77dd1c249094f79aea7a33b
pfcanales <pfcanales@gmail.com>
2022-10-28 10:03:16 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -6,11 +6,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.io.File;
+
 @Service
 public class UserService {  
     
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-       
+
+    
+    protected final File createTempDir(String prefix) {
+	try {
+		File tempDir = File.createTempFile(prefix + ".", "." + getPort());
+		tempDir.delete();
+		tempDir.mkdir();
+		tempDir.deleteOnExit();
+		return tempDir;
+	}
+    
     public List<User> getUser(String containName) {
         
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
